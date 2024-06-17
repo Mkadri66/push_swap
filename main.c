363c699da8bc7e889ecb638b1b3de1ae4c588d03
Mustapha Kadri <mkadri@c2r3p7.42perpignan.fr>
2024-06-17 16:16:57 +0200

    norm header and main

--- main.c
+++ main.c
@@ -6,23 +6,23 @@
 /*   By: mkadri <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 15:01:26 by mkadri            #+#    #+#             */
-/*   Updated: 2024/06/16 18:17:06 by mkadri           ###   ########.fr       */
+/*   Updated: 2024/06/17 15:10:00 by mkadri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    t_args  args;
+	t_args	args;
 
-    if(argc <= 1)
-        exit(1);
-    else if (argc == 2)
-        split_args(argv[1], &args);
-    else if (argc > 2)
-        parsing_args(argc, argv, &args);
-    check_input(&args);
-    free(args.params);
-    return(0);
+	if (argc <= 1)
+		exit (1);
+	else if (argc == 2)
+		split_args(argv[1], &args);
+	else if (argc > 2)
+		parsing_args(argc, argv, &args);
+	check_input(&args);
+	free(args.params);
+	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mkadri <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 15:02:07 by mkadri            #+#    #+#             */
-/*   Updated: 2024/06/16 18:12:42 by mkadri           ###   ########.fr       */
+/*   Updated: 2024/06/17 15:06:46 by mkadri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,21 +25,21 @@
 
 typedef struct s_args
 {
-    int *params;
-    int nb_params;
-}   t_args;
+	int	*params;
+	int	nb_params;
+}	t_args;
 
-int     parsing_args(int argc, char **argv, t_args *args);
-int     verif_is_int(char **argv);
-int     is_sorted(t_args *args);
-int     is_sign(char c);
-long	ft_atol(const char *str);
-int     check_double(t_args *args);
-int	    array_len(char **array);
-int     split_args(char *argv, t_args *args);
+int		parsing_args(int argc, char **argv, t_args *args);
+int		verif_is_int(char **argv);
+int		is_sorted(t_args *args);
+int		is_sign(char c);
+int		check_double(t_args *args);
+int		array_len(char **array);
+int		split_args(char *argv, t_args *args);
 void	free_array(char **array);
-void    display_error(char *str);
+void	display_error(char *str);
+void	check_input(t_args *args);
+long	ft_atol(const char *str);
 char	**verif_split(char *argv, t_args *args);
-void    check_input(t_args *args);
 
 #endif
